4a2b8ec983a0930e65c3e8dfbe4bab87dfaf0121
KristoferMar <kristofer@brostu.dk>
2020-03-30 00:23:12 +0200

    setup of routes

---
+++ routes/bootcamps.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Show all bootcamps'});
+});
+
+router.get('/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Show one bootcamp'});
+});
+
+router.post('/', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Create new bootcamps'});
+});
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Display bootcamp ${req.params.id}`});
+});
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}`});
+});
+
+module.exports = router;
