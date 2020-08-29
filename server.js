45351d0af5738a27344b5973d880f30773b7e382
KristoferMar <kristofer@brostu.dk>
2020-08-29 20:26:56 +0200

    Introduced controllers

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 
 // Route files
-const bootcamps = require('./routes/bootcamps');
+const bootcamps = require("./routes/bootcamps");
 
 // Load env vars
-dotenv.config({ path: './config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-// Mount routers 
-app.use('/api/v1/bootcamps', bootcamps);
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
