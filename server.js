f63d820a8b4ca2fd337030976517fa64646109a4
KristoferMar <kristofer@brostu.dk>
2020-08-29 23:53:21 +0200

    Connected applciation with mongoDB and got started on models

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const logger = require("./middleware/logger");
+const colors = require("colors");
 
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
+
+// Custom middleware import
+const logger = require("./middleware/logger");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -24,7 +32,16 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server a & exit process
+  server.close(() => process.exit(1));
+});
