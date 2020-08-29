a1751ecc780bcb4d55f3bad077c2264b23657af1
KristoferMar <kristofer@brostu.dk>
2020-08-29 20:48:29 +0200

    introduced middleware functions

---
+++ middleware/logger.js
@@ -0,0 +1,10 @@
+// Middleware
+// @desc    Logs request to consol
+const logger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+  next();
+};
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const logger = require("./middleware/logger");
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
@@ -8,6 +10,14 @@ const bootcamps = require("./routes/bootcamps");
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Make use of our middleware
+app.use(logger);
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
