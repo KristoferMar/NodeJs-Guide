9b7c567a7dffa56ebad9ff72ca07fb3fcdf4d9ac
KristoferMar <kristofer@brostu.dk>
2020-08-31 19:52:33 +0200

    Created a seeder and more

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -97,5 +99,33 @@ const BootcampSchema = new mongoose.Schema({
     default: Date.now
   }
 });
+
+// We can make use of slug to insert / create fields and models in database.
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function (next) {
+  // We can save any get any of these fields when we add in database.
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// This would be a geocoder.
+// BootcampSchema.pre('save', async function (next) {
+//   const loc = await geocoder.geocode(this.address);
+//   this.location = {
+//     type: 'Point',
+//     coordinates: [loc[0].longitude, loc[0].latitude],
+//     formattedAddress: loc[0].formattedAddress,
+//     street: loc[0].streetName,
+//     city: loc[0].city,
+//     state: loc[0].stateCode,
+//     zipcode: loc[0].zipcode,
+//     country: loc[0].countryCode,
+//   }
+
+//   // Do not save address in DB
+//   this.address = undefined;
+//   // next(); 
+// });
+
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
---
+++ seeder.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+const Bootcamp = require('./models/Bootcamp');
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+});
+
+// Read JSON files 
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+
+// Import into DB
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps);
+
+        console.log('Data Imported..'.green.inverse)
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+// Delete data
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+
+        console.log('Data destroyed..'.red.inverse)
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+if (process.argv[2] === '-i') {
+    importData();
+} else if (process.argv[2] === '-d') {
+    deleteData();
+}
